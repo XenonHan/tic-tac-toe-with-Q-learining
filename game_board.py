4132c6677943c305a7ad016ed0e13cca9bfffe93
Xenon <s10110040@gmail.com>
2022-01-04 02:16:13 +0800

    add GUI game board features

--- game_board.py
+++ game_board.py
@@ -4,14 +4,23 @@ This is the tic tac toc game board
 
 import future.moves.tkinter as tk
 import future.moves.tkinter.messagebox as mg
+from PIL import Image, ImageTk
+from ctypes import windll
+
+windll.shcore.SetProcessDpiAwareness(1)
 
 game_board = tk.Tk()
 game_board.title("Tic Tac Toc")
 game_turn = 0
 winner = False
-symbol = ['X', 'O']
+symbol = [('X','#BF3EFF'), ('O','#FF8C00')]
 player = []
 grids = []
+ai_pos = None
+human_pos = None
+theme = None
+ai_image = ImageTk.PhotoImage(Image.open('./img/robot.png').resize((50, 60)))
+human_image = ImageTk.PhotoImage(Image.open('./img/child.png').resize((45, 55)))
 
 
 # game terminate state, win/loss/tie
@@ -19,7 +28,7 @@ def game_terminate(btn1, btn2, btn3):
     for i in range(9):
         grids[i].config(state="disabled", cursor="")
         if i in [btn1, btn2, btn3]:
-            grids[i].config(bg="green")
+            grids[i].config(bg="lightgreen")
 
     print(game_turn)
     if winner:
@@ -34,8 +43,9 @@ def check_win():
     # check row & column
     for i in range(3):
         # check row
-        if grids[0 + i * 3]["text"] != " " and grids[0 + i * 3]["text"] == grids[1 + i * 3]["text"] == grids[2 + i * 3][
-            "text"]:
+        if grids[0 + i * 3]["text"] != " " and grids[0 + i * 3]["text"] == grids[1 + i * 3]["text"] == \
+                grids[2 + i * 3][
+                    "text"]:
             winner = True
             game_terminate(0 + i * 3, 1 + i * 3, 2 + i * 3)
             return
@@ -65,14 +75,9 @@ def check_win():
 def button_clicked(button):
     global game_turn
     if button["text"] == " ":
-        button.config(text=symbol[game_turn % 2], state="disabled", cursor="")
+        button.config(text=symbol[game_turn % 2][0], state="disabled", cursor="", disabledforeground=symbol[game_turn % 2][1])
         game_turn += 1
         check_win()
-
-
-# reset the game board
-def reset():
-    print("rest called")
 
 
 # Initialize list of buttons
@@ -81,36 +86,58 @@ def init_button():
         grids.append(
             tk.Button(game_board, text=" ", font=("Helvetica", 20), height=3, width=6, bg="silver", cursor="hand2",
                       disabledforeground="black", command=lambda but_num=i: button_clicked(grids[but_num])))
-        grids[i].grid(row=i // 3, column=i % 3)
+        grids[i].grid(row=(i + 3) // 3, column=i % 3)
 
 
 # Reset all the button
-def reset():
-    global game_turn
+def reset(ans):
+    global game_turn, winner
     for i in range(9):
-        grids[i].config(text=" ", font=("Helvetica", 20), height=3, width=6, bg="silver", cursor="hand2",
-                        disabledforeground="black", command=lambda but_num=i: button_clicked(grids[but_num]),
-                        state="normal")
+        grids[i].config(text=" ", bg="silver", cursor="hand2", state="normal")
     game_turn = 0
+    winner = False
+    play_order(ans)
 
 
-# init state of the game, prompt the game order
-def init():
-    global player
-    ans = mg.askyesno("Question", "Do you want to play first?")
+# decide the game order and display the img
+def play_order(ans=None):
+    global player, theme, ai_pos, human_pos
+    if ans is None:
+        mg.askyesno("Question", "Do you want to play first?")
     if ans:
         player = ["YOU", "AI"]
     else:
         player = ["AI", "YOU"]
 
+    if not theme:
+        theme = tk.Frame(game_board)
+        theme.grid(row=0, column=0, columnspan=3)
+        ai_pos = tk.Label(theme, image=ai_image)
+        ai_pos.grid(row=0, column=player.index('AI') * 2, padx=(player.index('AI') * 65, 0))
+        tk.Label(theme, text=": X", font=("Helvetica", 20), height=1, fg='#BF3EFF').grid(row=0, column=1)
+        human_pos = tk.Label(theme, image=human_image)
+        human_pos.grid(row=0, column=player.index('YOU') * 2, padx=(player.index('YOU') * 65, 0))
+        tk.Label(theme, text=": O", font=("Helvetica", 20), height=1, fg='#FF8C00').grid(row=0, column=3)
+    else:
+        ai_pos.grid(row=0, column=player.index('AI') * 2, padx=(player.index('AI') * 55, 0))
+        human_pos.grid(row=0, column=player.index('YOU') * 2, padx=(player.index('YOU') * 55, 0))
 
-# user menu
-topBar = tk.Menu(game_board, tearoff=False)
-topBar.add_command(label="RESET", command=lambda: reset())
-game_board.config(menu=topBar)
+
+# create user menu
+def init_menu():
+    top_bar = tk.Menu(game_board)
+    menu = tk.Menu(top_bar, tearoff=False)
+    menu.add_command(label="Reset as 1st", command=lambda: reset(1))
+    menu.add_command(label="Reset as 2nd", command=lambda: reset(0))
+    menu.add_command(label="Exit", command=lambda: exit(0))
+    top_bar.add_cascade(label="Select", menu=menu)
+    # topBar.add_command(label="RESET", command=lambda: reset())
+    game_board.config(menu=top_bar)
+
 
 # init
-# game_board.minsize(250, 200)
+init_menu()
 init_button()
-init()
+game_board.eval('tk::PlaceWindow . center')
+play_order()
 game_board.mainloop()
